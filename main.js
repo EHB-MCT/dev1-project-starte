e2ea7988eaf42147a94b0cb389e6d4689f04f30d
Joni De Borger <joni.de.borger@ehb.be>
2023-10-12 11:55:40 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+Voornaam Naam
+*/
+/** @type {CanvasRenderingContext2D} */
+let context;
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+context = canvas.getContext('2d');
+
+alert("Start");
